1b6cb9c1a3c59d0bb7bc4ef43c90e43e2ab5bf8c
gusauriemo <65194777+gusauriemo@users.noreply.github.com>
2022-03-28 19:04:55 +0100

    Update and rename model_non_gui2.py to Results_model.py

--- Results_model.py
+++ Results_model.py
@@ -1,19 +1,16 @@
 import nengo
-import nengo_loihi
 import matplotlib.pyplot as plt
 import numpy as np
 from datetime import datetime
 from scipy.io import loadmat
-from nengo.dists import Choice
 import itertools
-from nengo.utils.ensemble import response_curves, tuning_curves
 import scipy.io
 from subset import subsets
 from error import hearing_prediction, correct_prediction, detection_error_loss, distance_loss, sethares
 
 #File Paths
-full_path = "/Users/gustavo/Documents/Final Year Project/POLY3/full_poly3-A-Eb-C.mat" #i.e., the full dataset
-labels = "/Users/gustavo/Documents/Final Year Project/POLY3/labels_poly3-A-Eb-C.mat"
+full_path = "full_dataset_path.mat" #i.e., the full dataset
+labels = "label_dataset_path.mat"
 
 #Handling the data
 matrix_data = loadmat(full_path)
@@ -22,8 +19,6 @@ input_array = (np.log10(matrix_data["matrix"]))
 input_array[input_array<0] = 0
 output_array = (label_data["labels"])*3
 
-
-#print(output_array[(subsets(output_array, 30))]) # prints the new array with values up to index 30 (note 30)
 #Global variables
 nn= 30
 initial = 0
@@ -31,7 +26,7 @@ final = initial+nn
 D = nn
 N = D*50
 
-decoder_path = "/Users/gustavo/Documents/Final Year Project/codes/decoders_30_100s.mat"
+decoder_path = "file_from_trainable_model_path.mat"
 decoder_data = loadmat(decoder_path)
 decoders1 = decoder_data["decoders"]
 decoders = decoders1[0,:,:]
@@ -39,9 +34,6 @@ decoders = decoders1[0,:,:]
 sim_runtime = 5
 
 new_indexes = subsets(output_array, nn)
-
-#nengo_loihi.set_defaults() #https://www.nengo.ai/nengo-loihi/v1.1.0/overview.html
-
 
 with nengo.Network(seed=0) as net:
 
@@ -54,7 +46,6 @@ with nengo.Network(seed=0) as net:
     decision_ensemble = nengo.Ensemble(N*5, D, radius=4, seed=0)
 
     #Connections
-    #nengo.Connection(hearing_ensemble[:D], hearing_ensemble[D:], synapse=0.2)
     nengo.Connection(hearing_stim, hearing_ensemble[:D])
 
     #Direct neurons to ensemble connection
@@ -75,7 +66,6 @@ with nengo.Network(seed=0) as net:
 
 with nengo.Simulator(net) as sim:
     sim.run(sim_runtime)
-
     spike_count = np.sum(sim.data[spike_probe] > 0, axis=0) #total number of spikes in decision_ensemble during simulation
     spike_count2 = np.sum(sim.data[spike_probe] > 0, axis=0) #total number of spikes in hearing_ensemble
 
@@ -88,19 +78,15 @@ print(f"Average spikes per neuron per second: {np.round((number_spikes/number_ne
 
 
 #Error Calculations
-last= round((sim_runtime/(0.15))-1)
-hearing_sense = (hearing_prediction(sim.data[probe2]))[0:last]
-comparison = new_indexes[0:last]
+number_of_datapoints= round((sim_runtime/(0.15))-1)
+hearing_sense = (hearing_prediction(sim.data[probe2]))[0:number_of_datapoints]
+comparison = new_indexes[0:number_of_datapoints]
 expectation = (correct_prediction(output_array[comparison]))
 
-#print(f"Output of hearing: {hearing_sense}") #Compare this output of indexes from hearing to the indexes from input sound
 
-#print(f"Expected output: {expectation}")
-
-de_loss = (np.mean(detection_error_loss(expectation, hearing_sense)))
-distance_loss = (distance_loss(expectation, hearing_sense))
-#distance_loss1 = np.mean(distance_loss(expectation, hearing_sense))
-consonance_loss = np.mean(sethares(expectation, hearing_sense)[0])
+de_loss = (np.mean(detection_error_loss(expectation, hearing_sense))) #Detection-error loss
+distance_loss = (distance_loss(expectation, hearing_sense)) #Loss based on the distance in semitones
+consonance_loss = np.mean(sethares(expectation, hearing_sense)[0]) #Loss based on Plomp and Levelt's "Tonal Consonance"
 consonance_loss1 = np.mean(sethares(expectation, hearing_sense)[1])
 
 #Result Outputs
@@ -113,7 +99,7 @@ print(f"The accuracy under linear consonance loss is {np.round((1-consonance_los
 
 
 
-
+#Can plot the hearing signals vs Expected signals:
 
 # plt.figure()
 # plt.plot(sim.trange(), sim.data[probe4] , label="c")
