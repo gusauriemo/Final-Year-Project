453b3c6d6f46a5a63b7b8e7eb564068cb0813763
gusauriemo <65194777+gusauriemo@users.noreply.github.com>
2022-03-28 18:58:38 +0100

    Update and rename model_non_gui.py to Trainable_model.py
    Cleaned the code and added comments

--- Trainable_model.py
+++ Trainable_model.py
@@ -11,8 +11,8 @@ import scipy.io
 from subset import subsets
 
 
-full_path = "/Users/gustavo/Documents/Final Year Project/POLY3/full_poly3-A-Eb-C.mat"
-labels = "/Users/gustavo/Documents/Final Year Project/POLY3/labels_poly3-A-Eb-C.mat"
+full_path = "dataset_input_directory"
+labels = "label_dataset_directory"
 matrix_data = loadmat(full_path)
 label_data = loadmat(labels)
 
@@ -21,21 +21,19 @@ initial = 0
 final = initial+nn
 
 input_array = (np.log10(matrix_data["matrix"]))
-#print(np.max(input_array))
 input_array[input_array<0] = 0
 
-output_array = (label_data["labels"])*3
-#print(np.sum(output_array))
+output_array = (label_data["labels"])*3 #increase magnitude of labels
 
 D = nn #number of dimensions
 N = D*50 #smallest number of neurons per ensemble
 
-new_indexes = subsets(output_array, nn)
-#print(new_indexes)
+new_indexes = subsets(output_array, nn) #from subset file
 
-simulation_runtime = 100
+simulation_runtime = 100 
 
-
+#Important not to change seeds as they match the other model's
+#Model:
 with nengo.Network(seed=0) as net:
     #Correct signal - i.e., desired output
     correct_stim = nengo.Node(nengo.processes.PresentInput(output_array[new_indexes, initial:final], presentation_time=0.15))
@@ -45,28 +43,19 @@ with nengo.Network(seed=0) as net:
     hearing_stim = nengo.Node(nengo.processes.PresentInput(input_array[new_indexes, initial:final], presentation_time=0.15))
     hearing_ensemble = nengo.Ensemble(N*4, D, radius=4, seed=0)
     decision_ensemble = nengo.Ensemble(N*5, D, radius=4, seed=0)
-
+    error = nengo.Ensemble(N*5, D, radius = 4)
 
     # Connections
     nengo.Connection(correct_stim, correct_ensemble[:D])
     nengo.Connection(hearing_stim, hearing_ensemble[:D])
-    #nengo.Connection(hearing_ensemble[:D], hearing_ensemble[D:], synapse=0.2)
-    
     learn_conn = nengo.Connection(hearing_ensemble, decision_ensemble,  #can also use zero in the array instead of np.random.random(1)
         learning_rule_type = nengo.PES(learning_rate=8e-5)
         )
-
-    error = nengo.Ensemble(N*5, D, radius = 4)
 
     #Learning connections
     nengo.Connection(correct_ensemble, error, transform=-1)
     nengo.Connection(decision_ensemble, error, transform=1)
     nengo.Connection(error, learn_conn.learning_rule)
-
-
-    # -- inhibit errors after 40 seconds
-    #inhib = nengo.Node(lambda t: 2.0 if t > 60.0 else 0.0)
-    #nengo.Connection(inhib, error.neurons, transform=[[-1]] * error.n_neurons)
 
     #Probes
 
@@ -76,16 +65,12 @@ with nengo.Network(seed=0) as net:
     probe4 = nengo.Probe(decision_ensemble, "decoded_output", synapse=0.01)
     probe_weights = nengo.Probe(learn_conn, "weights", synapse=0.01, sample_every= simulation_runtime) #obtains weights at the end of the simulation
 
-#with net:
-    # Direct neurons to ensemble connection
-    #conn2 = nengo.Connection(ens1.neurons, ens2, transform=decoders)
-
 with nengo.Simulator(net) as sim:
     sim.run(simulation_runtime)
 
 decoders = (sim.data[probe_weights])
 
-scipy.io.savemat('decoders_30_100s.mat', mdict={'decoders': decoders})
+scipy.io.savemat('filename.mat', mdict={'decoders': decoders}) #Saving the decoder weigths
 
 #once the simulation is over, we can use these decoders to simply run the simlation directly (no learning) and evaluate the loss for both training and testing
 
@@ -97,13 +82,5 @@ plt.plot(sim.trange(), sim.data[probe1], label="c")
 plt.plot(sim.trange(), sim.data[probe4],label="b")
 plt.figure()
 plt.plot(sim.trange(), sim.data[probe3],label="b")
-
-#plt.legend(loc="best")
-
-# Find weight row with max variance
-#plt.figure()
-#neuron = np.argmax(np.mean(np.var(sim.data[probe_weights], axis=0), axis=1))
-#plt.plot(sim.trange(sample_every=simulation_runtime), sim.data[probe_weights][..., neuron])
-#plt.ylabel("Connection weight")
 
 plt.show()
